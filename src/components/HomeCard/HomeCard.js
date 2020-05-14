8231293736dca3bb8934dd4062a92fc3b24686ef
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-05-14 10:38:51 -0400

    finished home card

--- src/components/HomeCard/HomeCard.js
+++ src/components/HomeCard/HomeCard.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-const HomeCard = () => (
+const HomeCard = ({ graphic }) => (
     <div className="home-card">
-        <div className="home-card__header">
-            <div className="home-card__img-container">
-                <img src="https://source.unsplash.com/random?300x300?search=beach" alt="" className="home-card__img" />
-            </div>
+        <div className="home-card__img-container">
+            <img src={graphic} alt="" className="home-card__img" />
+        </div>
+        <div className="home-card__content">
             <div className="home-card__title">
                 Mission Statement
             </div>
-        </div>
-        <div className="home-card__content">
             <p className="home-card__text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis turpis urna, vitae venenatis justo mollis sit amet. Sed congue, urna eget mattis ullamcorper, orci magna consectetur nisl, et hendrerit libero ligula ac urna. Quisque pharetra libero justo. Nam vitae enim consequat, sodales purus ut, vestibulum purus. Donec euismod nibh quis ligula malesuada, ut scelerisque nunc vulputate. Praesent sed lobortis sapien. Pellentesque rhoncus odio consectetur justo pellentesque eleifend. Cras sit amet quam venenatis, fermentum elit vel, commodo magna. Proin non pretium metus, nec semper neque. Duis aliquam viverra condimentum. Aenean tincidunt dictum nulla, quis hendrerit velit condimentum id. Praesent luctus ultrices efficitur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nobis architecto quas ratione ad animi laudantium, neque rem consequuntur suscipit saepe natus impedit. Aut minima eos quibusdam consequatur quidem vitae suscipit modi magni tempora rem, voluptatibus quam dolorem incidunt aspernatur!
             </p>
         </div>
     </div>
--- src/components/HomeCard/home-card.stories.js
+++ src/components/HomeCard/home-card.stories.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import HomeCard from './HomeCard'
 
+import BusinessPlanSVG from '../../assets/images/business_plan.svg';
+import DataProcessingSVG from '../../assets/images/data_processing.svg';
+import PitchingSVG from '../../assets/images/pitching.svg';
+
 export default {
   title: 'HomeCard',
   component: HomeCard
 }
 
 export const BlankHomeCard = () => <HomeCard />
+export const PictureRight = () => <HomeCard graphic={BusinessPlanSVG} />
