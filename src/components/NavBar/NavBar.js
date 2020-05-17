24c63b9f27e2075ace9d30b4314c73f74be1f832
Marina Kun <mk3qa@virginia.edu>
2020-05-16 23:23:33 -0400

    Added banner and navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => (
     <div className="nav-bar">
-        <div className="nav-bar__container" style={{ backgroundImage: "linear-gradient(to right,#091D2A,#1C577F)" }}>
+        <div className="nav-bar__container" style={{ backgroundImage: "linear-gradient(to right,#103247,#1C577F)" }}>
             Contact Us
         </div>
         <div className="nav-bar__container" style={{ backgroundImage: "linear-gradient(to right,#1C577F,#5190BA)" }}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout/";
 import HomeCard from "../components/HomeCard/HomeCard"
-
+import NavBar from "../components/NavBar/NavBar"
 import BusinessPlanSVG from '../assets/images/business_plan.svg';
 import DataProcessingSVG from '../assets/images/data_processing.svg';
 import PitchingSVG from '../assets/images/pitching.svg';
-
+import Logo from '../assets/images/logo.png';
 export default () => (
   <Layout>
     <div className="home">
       {/* Banner here */}
+      <div className="home-card__banner-container">
+        <img src={Logo} className="home-card__logo" />
+      </div>
+      {/*
+      <div className="home-card__navbar">
+        <NavBar></NavBar>
+      </div>
+      */
+      }
       <div className="container">
         <HomeCard title="Business Plan" graphic={BusinessPlanSVG} imagePosition="left">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
