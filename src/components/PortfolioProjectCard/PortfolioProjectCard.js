d3f20e9ec70ef0f52ac15ceb3b3c2a0b1e149aaa
marinak6 <mk3qa@virginia.edu>
2021-07-16 16:34:41 -0400

    finished portfolio

--- src/components/PortfolioProjectCard/PortfolioProjectCard.js
+++ src/components/PortfolioProjectCard/PortfolioProjectCard.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import Slider from "react-slick";
-
-const PortfolioProjectCard = ({ graphic, title, description, impact, skills }) => {
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+const PortfolioProjectCard = ({ graphic, title, description, impact, skills, link }) => {
 
     const impactList = impact.map((bullets) => {
         return <li>{bullets}</li>;
@@ -26,38 +27,56 @@ const PortfolioProjectCard = ({ graphic, title, description, impact, skills }) =
                 Skills and techniques: {skillsList}
             </p>
 
-
         </div>
     )
 
     const settings = {
         dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
     };
+
+    let image;
     if (graphic.length == 1) {
-        let image = (
+        image = (
             <div className="portfolio-project-card__img-container">
                 <img src={graphic[0]} className="portfolio-project-card__img" />
+                {link &&
+                    <a href={link} className="portfolio-project-card__link">{link}</a>
+                }
             </div>
         )
     }
+
     else {
-        let image = () => (
-            <Slider {...settings}>
-                {graphic.map((pic) => {
-                    return (
-                        <div key={pic}>
-                            <h3>{pic}</h3>
-                        </div>
-                    );
-                })}
-            </Slider>
+        image = (
+            <div style={{
+                width: "45%",
+                height: "50%",
+                position: "relative",
+                color: "black"
+            }}>
+                <div style={{
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0
+
+                }}>
+                    <Slider {...settings}>
+                        {graphic.map((pic, index) => {
+                            return (
+                                <div key={index}>
+                                    <img src={pic} className="portfolio-project-card__img"></img>
+                                </div>
+                            );
+                        })}
+                    </Slider >
+                </div>
+                {link &&
+                    <a href={link}>{link}</a>
+                }
+            </div >
         );
     }
-
 
 
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import HomeCard from "../components/HomeCard"
+
 export default () => (
     <HomeCard></HomeCard>
 )
